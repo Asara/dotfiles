25477afe066a8eda6169937d66a860f1d151f73f
Asara <amarpreet@minhas.io>
2018-09-27 17:31:53 -0400

    remove uneeded files, update i3 config with proper usernames

--- X/cmatrixlock.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-i3-sensible-terminal -e cmatrix &
-xtrlock
-kill -9 $(pgrep cmatrix)
--- X/isslock.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-i3-sensible-terminal -e  livestreamer -Q http://ustream.tv/channel/iss-hdev-payload best --player 'mpv --fs ' &
-xtrlock
-kill -9 $(ps aux | grep 'mpv' | grep -v grep | awk '{print $2}')
